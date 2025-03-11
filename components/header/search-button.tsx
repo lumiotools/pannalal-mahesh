256fb2f279d2e65812bb7f1413e39dd9d2dd77c2
ABHIJEET VISHWAKARMA <abhijeetdrv@gmail.com>
2025-03-11 20:09:39 +0530

    add button

--- components/header/search-button.tsx
+++ components/header/search-button.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Search } from "lucide-react"
+import { Button } from "../ui/button"
 
 interface SearchButtonProps {
   isHome: boolean
@@ -9,14 +10,15 @@ interface SearchButtonProps {
 export default function SearchButton({ isHome }: SearchButtonProps) {
   return (
     <div className="p-2">
-      <button
-        title="Search"
+      <Button
+        // title="Search"
+        size={'icon'}
         className={`p-2 rounded-md transition-all duration-300 ${
           isHome ? "bg-transparent hover:bg-white/10" : "bg-white shadow hover:bg-gray-50"
         }`}
       >
         <Search className={`h-5 w-5 ${isHome ? "text-gray-100" : "text-gray-500"}`} />
-      </button>
+      </Button>
     </div>
   )
 }
